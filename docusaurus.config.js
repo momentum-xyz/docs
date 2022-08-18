84f84a73861ca5a650013cfcfc6d30f9f73e39ed
Jorrit <js@odyssey.org>
2022-08-18 12:11:55 +0200

    Try new gh-pages deploy method
    It's in "beta", might as well start using it for this project.
    
    So instead of a gh-pages branch, an custom action uploads an artifact.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'Momentum',
   tagline: 'Monumental',
   url: 'https://momentum.xyz',
-  baseUrl: '/',
+  baseUrl: process.env.BASE_URL || '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
